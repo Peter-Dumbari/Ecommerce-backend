0f0c24d350273d2b49c04ee812ee95090b75ac05
Peter Dumbari <dummytwiz@gmail.com>
2023-12-07 18:38:57 +0100

    fix the bug on order table

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ApplicationRecord
   validates :user, :order_date, :total_amount, presence: true
   validates :total_amount, numericality: { greater_than_or_equal_to: 0 }
   validate :order_date_in_past, if: -> { order_date.present? }
-  validate :custom_order_validation, if: -> { some_condition_is_met? }
+  # validate :custom_order_validation, if: -> { some_condition_is_met? }
 
   # Custom Validations
   def order_date_in_past
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_06_163957) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_07_164954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_163957) do
     t.decimal "total_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
